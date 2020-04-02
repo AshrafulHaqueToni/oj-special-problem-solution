7a796bfb9f1b25806f9d6e91aa54a235272386e1
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-04-03 01:03:40 +0600

    Update uva11488 - Hyper Prefix Sets.cpp

--- uva11488 - Hyper Prefix Sets.cpp
+++ uva11488 - Hyper Prefix Sets.cpp
@@ -1,4 +1,3 @@
-///একটা ডিকশনারিতে অনেকগুলো শব্দ আছে। এখন কোনো একটা শব্দ কয়বার “prefix” হিসাবে এসেছে সেটা বের করতে হবে
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,14 +5,17 @@ using namespace std;
 
 struct node
 {
-    map<int,node*>next;
+    node* next[2];
     bool endmark;
-    map<int,ll>mp;
+    ll mp[2];
     node()
     {
         endmark=false;
-        next.clear();
-        mp.clear();
+        for(int i=0; i<2; i++)
+        {
+            next[i]=NULL;
+            mp[i]=0;
+        }
 
     }
 }*root;
@@ -24,20 +26,19 @@ char ch[50005][205];
 void insert(char ch[],int len)
 {
     node* curr=root;
-    for(int i=0;i<len;i++)
+    for(int i=0; i<len; i++)
     {
-        int  s=ch[i]-'a';
-        if(curr->next.find(s)==curr->next.end())
+        int  s=ch[i]-'0';
+        if(curr->next[s]==NULL)
         {
             curr->next[s]=new node();
             curr->mp[s]=1;
-           // cout<<ch[i]<<endl;
         }
-        else curr->mp[s]+=1;
+        else
+            curr->mp[s]+=1;
 
         curr=curr->next[s];
 
-        //cout<<curr->mp[s]<<endl;
     }
     curr->endmark=true;
 }
@@ -46,14 +47,9 @@ ll cntword(char ch[],int len)
 {
     ll re=0;
     node* curr=root;
-    for(int i=0;i<len;i++)
+    for(int i=0; i<len; i++)
     {
-        int s=ch[i]-'a';
-        //cout<<curr->mp[s]<<endl;
-        if(curr->next.find(s)==curr->next.end())
-        {
-            return 0;
-        }
+        int s=ch[i]-'0';
         re=max(curr->mp[s]*(i+1),re);
         curr=curr->next[s];
     }
@@ -62,9 +58,10 @@ ll cntword(char ch[],int len)
 
 void del(node* root)
 {
-    for(int i=0;i<26;i++)
+    for(int i=0; i<2; i++)
     {
-        if(root->next.find(i)!=root->next.end())del(root->next[i]);
+        if(root->next[i]!=NULL)
+            del(root->next[i]);
     }
     delete(root);
 }
@@ -75,23 +72,22 @@ int main()
     scanf("%d",&t);
     while(t--)
     {
-    scanf("%d",&n);
-    root=new node();
-    for(int i=1;i<=n;i++)
-    {
-        scanf("%s",ch[i]);
-        insert(ch[i],strlen(ch[i]));
-    }
-    ll num=0;
-    for(int i=1;i<=n;i++)
-    {
-        ll val=cntword(ch[i],strlen(ch[i]));
-        //cout<<val<<endl;
-        num=max(num,val);
-    }
-    printf("%lld\n",num);
+        scanf("%d",&n);
+        root=new node();
+        for(int i=1; i<=n; i++)
+        {
+            scanf("%s",ch[i]);
+            insert(ch[i],strlen(ch[i]));
+        }
+        ll num=0;
+        for(int i=1; i<=n; i++)
+        {
+            ll val=cntword(ch[i],strlen(ch[i]));
+            num=max(num,val);
+        }
+        printf("%lld\n",num);
 
-    del(root);///destroy trie
+        del(root);///destroy trie
     }
 
     return 0;
